f290ef4ac74c9a40871961073bb1988868af8d1e
Megha P J <meghajayram30@gmail.com>
2023-12-30 10:55:34 +0530

    Add project files.

---
+++ Practice Project 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = ("E:\\Practice project 2//data.txt");
+
+            if (File.Exists(filePath))
+            {
+                foreach (string line in File.ReadAllLines(filePath))
+                {
+                    Console.WriteLine(line.Replace('\t', '\t'));
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found:");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
